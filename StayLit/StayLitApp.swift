ed98b08893a8bc1f4227e8ff6cabc7afe5835be7
Veyg <dawid.grochowski@veyg.me>
2023-10-12 09:56:29 +0200

    Initial Commit

---
+++ StayLit/StayLitApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayLitApp.swift
+//  StayLit
+//
+//  Created by Dawid Grochowski on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct StayLitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
