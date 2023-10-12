7fe41df47092f5c9e87ab97c2e5c26da98b7ee94
Veyg <dawid.grochowski@veyg.me>
2023-10-12 10:28:29 +0200

    buttons enabled

--- StayLit/StatusBarController.swift
+++ StayLit/StatusBarController.swift
@@ -5,4 +5,53 @@
 //  Created by Dawid Grochowski on 12/10/2023.
 //
 
-import Foundation
+import AppKit
+import SwiftUI
+import IOKit.pwr_mgt
+
+class StatusBarController {
+    private var statusBar: NSStatusItem?
+    private var assertionID: IOPMAssertionID = 0
+
+    init() {
+        statusBar = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        statusBar?.button?.image = NSImage(systemSymbolName: "moon.fill", accessibilityDescription: nil)
+        
+        let menu = NSMenu()
+
+        let enableItem = NSMenuItem(title: "Enable", action: #selector(toggleSleep), keyEquivalent: "")
+        enableItem.target = self
+        menu.addItem(enableItem)
+
+        let disableItem = NSMenuItem(title: "Disable", action: #selector(toggleSleep), keyEquivalent: "")
+        disableItem.target = self
+        menu.addItem(disableItem)
+
+        menu.addItem(NSMenuItem.separator())
+
+        let quitItem = NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q")
+        quitItem.target = self
+        menu.addItem(quitItem)
+
+        statusBar?.menu = menu
+    }
+    @objc func toggleSleep() {
+        if assertionID == 0 {
+            IOPMAssertionCreateWithName(kIOPMAssertionTypeNoDisplaySleep as CFString,
+                                        IOPMAssertionLevel(kIOPMAssertionLevelOn),
+                                        "Prevent Sleep" as CFString,
+                                        &assertionID)
+            statusBar?.button?.image = NSImage(systemSymbolName: "sun.max.fill", accessibilityDescription: nil) // Sun icon when on
+            statusBar?.button?.title = "On"
+        } else {
+            IOPMAssertionRelease(assertionID)
+            assertionID = 0
+            statusBar?.button?.image = NSImage(systemSymbolName: "moon.fill", accessibilityDescription: nil) // Moon icon when off
+            statusBar?.button?.title = "Off"
+        }
+    }
+    @objc func quitApp() {
+        NSApplication.shared.terminate(nil)
+    }
+
+}
--- StayLit/StayLitApp.swift
+++ StayLit/StayLitApp.swift
@@ -4,14 +4,22 @@
 //
 //  Created by Dawid Grochowski on 12/10/2023.
 //
-
 import SwiftUI
-
 @main
 struct StayLitApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusBarController: StatusBarController?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusBarController = StatusBarController()
     }
 }
