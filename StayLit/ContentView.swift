3a0103db3058c384dfdf25da46371b3c17462dfe
Dawid Grochowski <dawid.grochowski@veyg.me>
2023-10-16 01:19:17 +0200

    UI changes: - Box size - Middled text

--- StayLit/ContentView.swift
+++ StayLit/ContentView.swift
@@ -4,54 +4,68 @@ import LaunchAtLogin
 struct ContentView: View {
     @State private var autoCheckForUpdates: Bool = UserDefaults.standard.bool(forKey: "autoCheckForUpdates")
 
+    func appVersion() -> String {
+        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String,
+           let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String {
+            return "Version \(version) (\(build))"
+        }
+        return "Unknown Version"
+    }
+
     var body: some View {
         VStack(spacing: 30) {
-            Text("StayLit Settings")
+            Text("Settings")
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .padding(.top, 40)
 
             GroupBox(label: HStack {
+                Spacer()
                 Image(systemName: "gear")
                 Text("General")
                     .fontWeight(.medium)
+                Spacer()
             }) {
-                VStack(alignment: .leading, spacing: 10) {
+                VStack(spacing: 10) {
                     Toggle(isOn: $autoCheckForUpdates) {
                         Label("Automatically Check for updates", systemImage: "arrow.triangle.2.circlepath.circle")
                     }
                     .toggleStyle(SwitchToggleStyle(tint: .blue))
-                    .padding(.vertical, 5)
                 }
+                .frame(maxWidth: .infinity)
                 .padding()
             }
 
             GroupBox(label: HStack {
+                Spacer()
                 Image(systemName: "power")
                 Text("Startup")
                     .fontWeight(.medium)
+                Spacer()
             }) {
-                VStack(alignment: .leading, spacing: 10) {
+                VStack(spacing: 10) {
                     LaunchAtLogin.Toggle()
                         .toggleStyle(SwitchToggleStyle(tint: .blue))
-                        .padding(.vertical, 5)
                 }
+                .frame(maxWidth: .infinity)
                 .padding()
             }
 
             GroupBox(label: HStack {
+                Spacer()
                 Image(systemName: "info.circle")
                 Text("About")
                     .fontWeight(.medium)
+                Spacer()
             }) {
-                VStack(alignment: .leading, spacing: 10) {
+                VStack(spacing: 10) {
                     Button(action: {
                         // Open website
                         if let url = URL(string: "https://veyg.me") {
                             NSWorkspace.shared.open(url)
                         }
                     }) {
-                        Label("Visit Our Website", systemImage: "globe")
+                        Label("Visit My Website", systemImage: "globe")
                     }
                     .buttonStyle(PlainButtonStyle())
 
@@ -66,9 +80,15 @@ struct ContentView: View {
                     .buttonStyle(PlainButtonStyle())
                     .foregroundColor(.red)
                 }
+                .frame(maxWidth: .infinity)
                 .padding()
             }
-
+            Text(appVersion())
+                .font(.footnote)
+                .foregroundColor(.gray)
+            Text("Developed with ❤️ by Veyg")
+                .font(.caption)
+                .foregroundColor(.gray)
             Spacer()
         }
         .padding([.horizontal, .bottom])
