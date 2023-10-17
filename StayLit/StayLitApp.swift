aae7aa18ca6f6707180b45f2d6ffad58d1056586
Dawid Grochowski <dawid.grochowski@veyg.me>
2023-10-17 15:04:05 +0200

    App hidden from dock

--- StayLit/StayLitApp.swift
+++ StayLit/StayLitApp.swift
@@ -5,6 +5,7 @@
 //  Created by Dawid Grochowski on 12/10/2023.
 //
 import SwiftUI
+import AppKit
 
 @main
 struct StayLitApp: App {
@@ -17,13 +18,16 @@ struct StayLitApp: App {
     }
 }
 
-
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusBarController: StatusBarController?
+    
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return false
     }
+    
     func applicationDidFinishLaunching(_ notification: Notification) {
+        hideFromDock()
+        
         statusBarController = StatusBarController()
         
         if let window = NSApplication.shared.windows.first {
@@ -32,4 +36,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             window.setContentSize(NSSize(width: 400, height: 500))
         }
     }
+    
+    func hideFromDock() {
+        var psn = ProcessSerialNumber(highLongOfPSN: 0, lowLongOfPSN: UInt32(kCurrentProcess))
+        let transformState = ProcessApplicationTransformState(kProcessTransformToUIElementApplication)
+        TransformProcessType(&psn, transformState)
+    }
+
 }
